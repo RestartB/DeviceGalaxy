46018a70f1570ba8eb948ef6715c730c10537c8e
RestartB <79792966+RestartB@users.noreply.github.com>
2026-01-15 19:35:21 +0000

    fix: change to correct type, remove base url from auth client

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -3,7 +3,6 @@ import { twoFactorClient, adminClient } from 'better-auth/client/plugins';
 import { goto } from '$app/navigation';
 
 export const authClient = createAuthClient({
-  baseURL: 'https://devicegalaxy.me',
   plugins: [
     twoFactorClient({
       onTwoFactorRedirect() {
--- src/routes/dash/admin/+page.server.ts
+++ src/routes/dash/admin/+page.server.ts
@@ -1,8 +1,8 @@
 import { auth } from '$lib/server/auth';
 import { redirect } from '@sveltejs/kit';
-import type { LayoutServerLoad } from './$types';
+import type { PageServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async (event) => {
+export const load: PageServerLoad = async (event) => {
   const session = await auth.api.getSession({
     headers: event.request.headers
   });
